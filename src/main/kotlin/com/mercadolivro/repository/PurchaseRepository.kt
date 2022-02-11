3457c7faa87333a3e190c068ae56a4239c8f69cf
Rogerio Augusto Custodio <rogerioaugustocustodio@gmail.com>
2022-02-11 05:47:42 -0300

    Criando endpoint para vendas e listeners para atualizacao do status dos livros vendidos e criacao de nota fiscal

---
+++ src/main/kotlin/com/mercadolivro/repository/PurchaseRepository.kt
@@ -0,0 +1,10 @@
+package com.mercadolivro.repository
+
+import com.mercadolivro.model.PurchaseModel
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PurchaseRepository : CrudRepository<PurchaseModel, Int> {
+
+}
--- src/main/kotlin/com/mercadolivro/service/BookService.kt
+++ src/main/kotlin/com/mercadolivro/service/BookService.kt
@@ -59,4 +59,15 @@ class BookService(
         bookRepository.saveAll(books)
     }
 
+    fun findAllByIds(bookIds: Set<Int>): MutableList<BookModel> {
+        return bookRepository.findAllById(bookIds)
+    }
+
+    fun purchase(books: MutableList<BookModel>) {
+        books.map {
+            it.status = BookStatus.VENDIDO
+        }
+        bookRepository.saveAll(books)
+    }
+
 }
