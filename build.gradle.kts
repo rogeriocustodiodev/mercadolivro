2f866672a0a9d4c93f868685bb2bc91667751c12
Rogerio Augusto Custodio <rogerioaugustocustodio@gmail.com>
2022-02-20 05:54:15 -0300

    Iniciando a criacao de testes unitarios com MockK

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
 	runtimeOnly("mysql:mysql-connector-java")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.mockk:mockk:1.12.2")
 }
 
 tasks.withType<KotlinCompile> {
