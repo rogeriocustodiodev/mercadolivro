ce51bef0b627ee0bb6274b3d2d3e08f643f10405
Rogerio Augusto Custodio <rogerioaugustocustodio@gmail.com>
2022-02-22 05:31:26 -0300

    Concluindo testes do CustomerService

--- src/test/kotlin/com/mercadolivro/service/CustomerServiceTest.kt
+++ src/test/kotlin/com/mercadolivro/service/CustomerServiceTest.kt
@@ -9,9 +9,12 @@ import com.mercadolivro.repository.CustomerRepository
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.SpyK
 import io.mockk.junit5.MockKExtension
+import io.mockk.just
+import io.mockk.runs
 import io.mockk.verify
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
@@ -34,6 +37,7 @@ class CustomerServiceTest {
     private lateinit var bCrypt: BCryptPasswordEncoder
 
     @InjectMockKs
+    @SpyK
     private lateinit var customerService: CustomerService
 
     @Test
@@ -118,16 +122,148 @@ class CustomerServiceTest {
         verify(exactly = 1) { customerRepository.findById(id) }
     }
 
+    @Test
+    fun `should throw exception when customer to be updated not exists`() {
+        // Arrange
+        val id = Random().nextInt()
+        val fakeCustomer = buildCustomer(id = id)
+        every { customerRepository.existsById(id) } returns false
+
+        // Act
+        val error = assertThrows<NotFoundException> {
+            customerService.update(fakeCustomer)
+        }
+
+        // Assert
+        assertEquals("Customer [${id}] not exists", error.message)
+        assertEquals("ML-201", error.errorCode)
+        verify(exactly = 1) { customerRepository.existsById(id) }
+        verify(exactly = 0) { customerRepository.save(any()) }
+    }
+
+    @Test
+    fun `should update customer`() {
+        // Arrange
+        val id = Random().nextInt()
+        val fakeCustomer = buildCustomer(id = id)
+        every { customerRepository.existsById(id) } returns true
+        every { customerRepository.save(fakeCustomer) } returns fakeCustomer
+
+        // Act
+        customerService.update(fakeCustomer)
+
+        // Assert
+        verify(exactly = 1) { customerRepository.existsById(id) }
+        verify(exactly = 1) { customerRepository.save(fakeCustomer) }
+    }
+
+    @Test
+    fun `should delete customer`() {
+        // Arrange
+        val id = Random().nextInt()
+        val fakeCustomer = buildCustomer(id = id)
+        val expectedCustomer = fakeCustomer.copy(status = CustomerStatus.INATIVO)
+        every { customerService.findById(id) } returns fakeCustomer
+        every { bookService.deleteByCustomer(fakeCustomer) } just runs
+        every { customerRepository.save(expectedCustomer)} returns expectedCustomer
+
+        // Act
+        customerService.delete(id)
+
+        // Assert
+        verify(exactly = 1) { customerService.findById(id) }
+        verify(exactly = 1) { bookService.deleteByCustomer(fakeCustomer) }
+        verify(exactly = 1) { customerRepository.save(expectedCustomer) }
+    }
+
+    @Test
+    fun `should throw not found exception when delete customer`() {
+        // Arrange
+        val id = Random().nextInt()
+        every { customerService.findById(id) } throws NotFoundException(Errors.ML201.message.format(id), Errors.ML201.code)
+
+        // Act
+        val error = assertThrows<NotFoundException> {
+            customerService.delete(id)
+        }
+
+        // Assert
+        assertEquals("Customer [${id}] not exists", error.message)
+        assertEquals("ML-201", error.errorCode)
+        verify(exactly = 1) { customerService.findById(id) }
+        verify(exactly = 0) { bookService.deleteByCustomer(any()) }
+        verify(exactly = 0) { customerRepository.save(any()) }
+    }
+
+    @Test
+    fun `should return true when email available`() {
+        // Arrange
+        val email = "${Random().nextInt()}@email.com"
+        every { customerRepository.existsByEmail(email) } returns false
+
+        // Act
+        val emailAvailable = customerService.emailAvailable(email)
+
+        // Assert
+        assertTrue(emailAvailable)
+        verify(exactly = 1) { customerRepository.existsByEmail(email) }
+    }
+
+    @Test
+    fun `should return false when email unavailable`() {
+        // Arrange
+        val email = "${Random().nextInt()}@email.com"
+        every { customerRepository.existsByEmail(email) } returns true
+
+        // Act
+        val emailAvailable = customerService.emailAvailable(email)
+
+        // Assert
+        assertFalse(emailAvailable)
+        verify(exactly = 1) { customerRepository.existsByEmail(email) }
+    }
+
+    @Test
+    fun `should return true when customer is active`() {
+        // Arrange
+        val id = Math.random().toInt()
+        val fakeCustomer = buildCustomer(id = id)
+        every { customerService.findById(id) } returns fakeCustomer
+
+        // Act
+        val isCustomerActive = customerService.isCustomerActive(id)
+
+        // Assert
+        assertTrue(isCustomerActive)
+        verify(exactly = 1) { customerService.isCustomerActive(id) }
+    }
+
+    @Test
+    fun `should return false when customer is inactive`() {
+        // Arrange
+        val id = Math.random().toInt()
+        val fakeCustomer = buildCustomer(id = id, status = CustomerStatus.INATIVO)
+        every { customerService.findById(id) } returns fakeCustomer
+
+        // Act
+        val isCustomerActive = customerService.isCustomerActive(id)
+
+        // Assert
+        assertFalse(isCustomerActive)
+        verify(exactly = 1) { customerService.isCustomerActive(id) }
+    }
+
     private fun buildCustomer(
        id: Int? = null,
        name: String = "customer name",
        email: String = "${UUID.randomUUID()}@email.com",
+       status: CustomerStatus = CustomerStatus.ATIVO,
        password: String = "password"
     ) = CustomerModel(
         id = id,
         name = name,
         email = email,
-        status = CustomerStatus.ATIVO,
+        status = status,
         password = password,
         roles = setOf(Role.CUSTOMER)
     )
